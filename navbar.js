643d27097424993256395069ef0b68fa6acb840b
celestinosalim <celestino.salim@gmail.com>
2019-06-26 18:01:39 -0400

    navbar media queries done

--- navbar.js
+++ navbar.js
@@ -1,6 +1,8 @@
 $(function() {
   $(document).scroll(function() {
     var $nav = $(".navbar-ul");
-    $nav.toggleClass("scrolled", $(this).scrollTop() > 100);
+    $nav.toggleClass("scrolled-1", $(this).scrollTop() > 75);
+    $nav.toggleClass("scrolled-2", $(this).scrollTop() > 500);
+    $nav.toggleClass("scrolled-3", $(this).scrollTop() > 700);
   });
 });
