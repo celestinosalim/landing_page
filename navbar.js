042c6907bf3bacc25b261634304cad7a7b9c2bdd
celestinosalim <celestino.salim@gmail.com>
2019-06-27 00:53:18 -0400

    added logo color class

--- navbar.js
+++ navbar.js
@@ -1,8 +1,10 @@
 $(function() {
   $(document).scroll(function() {
     var $nav = $(".menu-content");
+    var $logo = $("#logo");
     $nav.toggleClass("scrolled-1", $(this).scrollTop() > 75);
     $nav.toggleClass("scrolled-2", $(this).scrollTop() > 500);
     $nav.toggleClass("scrolled-3", $(this).scrollTop() > 715);
+    $logo.toggleClass("logo-color", $(this).scrollTop() > 715);
   });
 });
